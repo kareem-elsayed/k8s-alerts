9e505240fd7a3bc840042ea07507a4c83fea4a3e
kareem-elsayed <kareem.elsayed@fly365.com>
2020-05-15 06:40:36 +0200

    using yield insted of using list array

--- pods.py
+++ pods.py
@@ -4,26 +4,30 @@ from operator import itemgetter
 
 class Pod:
     def __init__(self):
-        data = subprocess.check_output('kubectl get pods --all-namespaces', shell=True)
+        data = subprocess.check_output('kubectl get pods --all-namespaces', shell = True)
         data_decode = data.decode('ASCII')
         self.obj = data_decode.split()
         self.err_msg_list = [
-                                "ErrImagePull",
-                                "Error",
-                                "CrashLoopBackOff",
-                                "ImagePullBackOff",
-                            ]
+            "ErrImagePull",
+            "Error",
+            "CrashLoopBackOff",
+            "ImagePullBackOff",
+        ]
+
+    # def filtering_data(self):
+    #     new = []
+    #     for i in range(0, len(self.obj), 6):
+    #         new.append(self.obj[i: i + 6])
+    #     return new
 
     def filtering_data(self):
-        new = []
         for i in range(0, len(self.obj), 6):
-            new.append(self.obj[i: i + 6])
-        return new
+            yield (self.obj[i: i + 6])
 
     def catching_errors(self, list_of_pods):
-        for e in self.err_msg_list:
-            for i in list_of_pods:
-                if e in i:
-                    data = itemgetter(0, 1, 3)(i)  # select specific items from list of the pods
+        for items in list_of_pods:
+            for err in self.err_msg_list:
+                if err in items:
+                    data = itemgetter(0, 1, 3)(items)  # select specific items from list of the pods
                     data_str = '    '.join(data)  # concatenate and convert items to strings
                     print(data_str)
